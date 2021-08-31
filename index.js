182c71f0da42d325ede63356740cfa1e05ec2374
Frankma17 <frankin.canon@uptc.edu.co>
2021-08-31 15:22:47 -0500

    Add index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const port = 3200
+let status = 202;
+
+app.get('/', (req, res) => {
+
+  let option = random(random(0,1),1)
+  if (status === 202) {
+    if (option === 1) {
+      res.status(202).send('ERROR 202');
+      status = 202;
+      console.log("ERROR 202")
+      console.log(status);
+    } else {
+      res.status(500).send('ERROR 500');
+      status = 500;
+      console.log("ERROR 500");
+      console.log(status);
+    }
+  }else{
+    res.status(500).send('ERROR 500');
+    status = 500;
+    console.log("ERROR 500");
+    console.log(status);
+  }
+})
+
+function random(inferior, superior) {
+  var possibilities = superior - inferior;
+  var random = Math.random() * (possibilities + 1);
+  random = Math.floor(random);
+  return inferior + random;
+}
+
+app.listen(port, () => {
+  console.log(`Listening http://localhost:${port}`)
+})
